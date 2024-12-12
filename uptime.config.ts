95619fca76465a6c5d6262d0c823a1067875ffb8
iobb <yangjbbb@gmail.com>
2024-12-12 23:46:05 +0800

    更新 uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -76,12 +76,12 @@ const workerConfig = {
       timeout: 5000,
     },
     {
-      id: 'plhy2',
-      name: 'Pl-hy2',
+      id: 'plvless',
+      name: 'Pl-Vless',
       // `method` should be `TCP_PING` for tcp monitors
       method: 'TCP_PING',
       // `target` should be `host:port` for tcp monitors
-      target: '128.204.223.118:13137',
+      target: '128.204.223.118:14697',
       tooltip: '我的生产服务器SSH',
       statusPageLink: 'https://bcn.us.kg',
       timeout: 5000,
