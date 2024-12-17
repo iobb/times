a099b3dea5253c16a8932d69bb13bbddff6a4750
iobb <yangjbbb@gmail.com>
2024-12-17 15:54:49 +0800

    更新 uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -81,7 +81,7 @@ const workerConfig = {
       // `method` should be `TCP_PING` for tcp monitors
       method: 'TCP_PING',
       // `target` should be `host:port` for tcp monitors
-      target: '128.204.223.117:14697',
+      target: '128.204.233.118:22',
       tooltip: '我的生产服务器SSH',
       statusPageLink: 'https://bcn.us.kg',
       timeout: 5000,
